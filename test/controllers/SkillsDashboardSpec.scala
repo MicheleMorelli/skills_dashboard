00dc13015d9550487dc58347d3223b6cb62df7db
MicheleMorelli <morelli.michele@hotmail.it>
2020-07-01 10:28:55 +0100

    adding some model classes

--- test/controllers/SkillsDashboardSpec.scala
+++ test/controllers/SkillsDashboardSpec.scala
@@ -5,4 +5,7 @@ import org.scalatest._
 import org.scalatest.matchers.should.Matchers
 
 class SkillsDashboardSpec extends FlatSpec with Matchers {
+  "this test" should "definitely pass" in {
+    true should be(true)
+  }
 }
